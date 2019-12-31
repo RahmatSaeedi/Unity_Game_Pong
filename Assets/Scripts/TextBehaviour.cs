320c9b2881a482d4186a92fd885a74f972eead7f
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-12-30 22:10:58 -0700

    menue transitions

---
+++ Assets/Scripts/TextBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBehaviour : MonoBehaviour
+{
+    public Text oText;
+    int textNumber = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            textNumber++;
+            oText.text = textNumber.ToString();
+        } else if (Input.GetKeyDown(KeyCode.S)){
+            oText.text = "S";
+        }
+    }
+}
