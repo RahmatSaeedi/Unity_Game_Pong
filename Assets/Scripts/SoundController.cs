d38011b27609c34d1f1be6f475c77ab983d627f7
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2020-01-01 13:30:12 -0700

    +sounds

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource wallSound;
+    public AudioSource racketSound;
+    public AudioSource gameResetSound;
+    public AudioSource gameOverSound;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch(collision.gameObject.name)
+        {
+            case "RacketPlayer1":
+                this.racketSound.Play();
+                break;
+            case "RacketPlayer2":
+                this.racketSound.Play();
+                break;
+            case "WallLeft":
+                this.wallSound.Play();
+                break;
+            case "WallRight":
+                this.wallSound.Play();
+                break;
+        }
+    }
+
+}
